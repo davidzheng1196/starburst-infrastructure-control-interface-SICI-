0292817fc088e555e6f8be0f9fa58b9fe8e9abaf
davidzheng1196 <zxy617375527@gmail.com>
2026-02-23 11:16:03 -0600

    initialize

---
+++ src/main/java/com/jpmc/starburst/sici/JpmcStarburstInfrastructureControlInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.jpmc.starburst.sici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmcStarburstInfrastructureControlInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpmcStarburstInfrastructureControlInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpmc/starburst/sici/JpmcStarburstInfrastructureControlInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmc.starburst.sici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpmcStarburstInfrastructureControlInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
